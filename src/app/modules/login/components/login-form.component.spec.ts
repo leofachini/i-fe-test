0b3b6172f1255804f1624c57b92aaa2e2e6b99fd
Leonardo F. Martins <leonardo.martins@navita.com.br>
2020-05-25 17:48:34 +0100

    Created LoginForm component tests

---
+++ src/app/modules/login/components/login-form.component.spec.ts
@@ -0,0 +1,69 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+import { LoginFormComponent } from './login-form.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+describe('LoginFormComponent', () => {
+  let comp: LoginFormComponent;
+  let fixture: ComponentFixture<LoginFormComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        SharedModule,
+      ],
+      declarations: [
+        LoginFormComponent,
+      ],
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(LoginFormComponent);
+      de = fixture.debugElement;
+      comp = de.componentInstance;
+      el = de.nativeElement;
+    });
+  }));
+
+  it('should create the LoginForm component', () => {
+    expect(comp).toBeTruthy();
+  });
+
+  it('should have a form tag', () => {
+    fixture.detectChanges();
+
+    const form = de.query(By.css('form#loginForm')).nativeElement;
+    expect(form).toBeTruthy();
+  });
+
+  it('should have a username input', () => {
+    fixture.detectChanges();
+
+    const labelText = 'Username';
+
+    const usernameInput = de.query(By.css('form#loginForm mat-form-field input#username')).nativeElement;
+    const usernameLabel = de.query(By.css('form#loginForm mat-form-field mat-label[for=username]')).nativeElement;
+
+    expect(usernameInput).toBeTruthy();
+    expect(usernameLabel).toBeTruthy();
+    expect(usernameLabel.textContent).toEqual(labelText);
+  });
+
+  it('should have a password input', () => {
+    fixture.detectChanges();
+
+    const labelText = 'Password';
+
+    const passwordInput = de.query(By.css('form#loginForm mat-form-field input#password')).nativeElement;
+    const passwordLabel = de.query(By.css('form#loginForm mat-form-field mat-label[for=password]')).nativeElement;
+
+    expect(passwordInput).toBeTruthy();
+    expect(passwordLabel).toBeTruthy();
+    expect(passwordLabel.textContent).toEqual(labelText);
+  });
+
+});
