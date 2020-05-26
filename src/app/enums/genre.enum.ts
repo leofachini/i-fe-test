01276e8a6916abe89efb84336a08d2fe244e9e97
Leonardo F. Martins <leonardo.martins@navita.com.br>
2020-05-26 12:51:49 +0100

    Created genre enum

---
+++ src/app/enums/genre.enum.ts
@@ -0,0 +1,13 @@
+export enum Genre {
+  Action = 'ACTION',
+  Adventure = 'ADVENTURE',
+  Comedy = 'COMEDY',
+  Crime = 'CRIME',
+  Drama = 'DRAMA',
+  Fantasy = 'FANTASY',
+  Fiction = 'FICTION',
+  Historical = 'HISTORICAL',
+  Horrror = 'HORROR',
+  Mistery = 'MISTERY',
+  SciFi = 'SCI-FI',
+}
