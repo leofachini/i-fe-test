623299e8b68dc8069ef960bd4a1b6d8d5aad47f6
Leonardo F. Martins <leonardo.martins@navita.com.br>
2020-05-31 15:10:51 +0100

    Set the last five watched movies in the profiles

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -3,19 +3,28 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { cloneDeep, find } from 'lodash-es';
 import { get as idbGet, set as idbSet } from 'idb-keyval';
 
+import { MovieService } from './movie.service';
 import { Profile } from '../models';
 
 @Injectable()
 export class ProfileService {
 
   private _activeProfileBehaviorSubject: BehaviorSubject<Profile> = new BehaviorSubject(undefined);
-  private _defaultProfiles = [
-    new Profile('bob', 'Bob', 'bb@bob.us', 'bob-dylan-profile.jpg', []),
-    new Profile('kate', 'Kate', 'kt@kate.la', 'kate-moss-profile.jpeg', []),
-  ];
-  private _profilesBehaviorSubject: BehaviorSubject<Profile[]> = new BehaviorSubject(this._defaultProfiles);
+  private _profilesBehaviorSubject: BehaviorSubject<Profile[]>;
 
-  constructor() {
+  constructor(
+    private _movieService: MovieService,
+  ) {
+    idbGet('profiles').then((profiles: Profile[]) => {
+      if (!profiles) {
+        profiles =  [
+          new Profile(1, 'bob', 'Bob', 'bb@bob.us', 'bob-dylan-profile.jpg', this._movieService.getRandomMovies(5)),
+          new Profile(2, 'kate', 'Kate', 'kt@kate.la', 'kate-moss-profile.jpeg', this._movieService.getRandomMovies(5)),
+        ];
+        idbSet('profiles', profiles);
+      }
+      this._profilesBehaviorSubject = new BehaviorSubject(profiles);
+    })
     idbGet('activeProfile').then(this.setActiveProfile.bind(this));
   }
 
@@ -29,10 +38,6 @@ export class ProfileService {
 
   getProfiles(): Profile[] {
     return this._profilesBehaviorSubject.getValue();
-  }
-
-  setProfiles(profiles: Profile[] = this._defaultProfiles): void {
-    this._profilesBehaviorSubject.next(profiles);
   }
 
   getActiveProfile(): Profile {
