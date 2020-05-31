d60675be0d5aa29e4509ee236968dd7c93eda2a5
Leonardo F. Martins <leonardo.martins@navita.com.br>
2020-05-31 14:15:31 +0100

    Implementations into profile page
    - Added logout option
    - Added profile photo
    - Added Profile info with Name and Email

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { LogoutDialogComponent } from './components/logout-dialog.component';
 import { ProfilePage } from './profile.page';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
+    LogoutDialogComponent,
     ProfilePage,
   ],
   imports: [
@@ -17,5 +19,6 @@ import { SharedModule } from 'src/app/shared/shared.module';
     }]),
     SharedModule,
   ],
+  entryComponents: [LogoutDialogComponent],
 })
 export class ProfileModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,34 @@ import { CommonModule } from '@angular/common';
 import {
   MatButton,
   MatButtonModule,
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardModule,
+  MatCardTitle,
+  MatDialogClose,
+  MatDialogModule,
+  MatError,
   MatFormField,
   MatFormFieldModule,
-  MatError,
   MatInput,
   MatInputModule,
   MatLabel,
   MatSnackBarModule,
 } from '@angular/material';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 const COMPONENTS = [
   MatButton,
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardTitle,
+  MatDialogClose,
   MatError,
   MatFormField,
   MatInput,
@@ -27,14 +41,18 @@ const COMPONENTS = [
   imports: [
     CommonModule,
     FontAwesomeModule,
+    FormsModule,
     ReactiveFormsModule,
     MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
     MatSnackBarModule,
   ],
   exports: [
     FontAwesomeModule,
+    FormsModule,
     ReactiveFormsModule,
     ...COMPONENTS,
   ],
